039a11b51a239d06a33bb336eeb30f3446dea2ff
IvanPolitov Work <ivan.hanter21@mail.ru>
2024-03-06 19:28:51 +0500

    Отправляет сообщение в чат со мной раз в секунду

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import re
-"""
-token = ""
+import requests
+import time
 
+TOKEN = ""
+API_URL = "https://api.telegram.org/bot"
 with open("secret.txt", "r") as q:
     text = q.read()
-    token = re.match(r'token:([:\w-]*$)', text).group(1)
-
-"""
+    TOKEN = re.match(r'token:([:\w-]*$)', text).group(1)
+for i in range(1000):
+    updates = requests.get(f'{API_URL}{TOKEN}/sendMessage?chat_id=1393291388&text=QQQQQQQ')
+    print(updates.text)
+    time.sleep(1)
