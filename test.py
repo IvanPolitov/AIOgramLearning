e46f1f4fe2181ccc23f5204d10591dc013905ceb
Ivan Politov <ivan.hanter21@mail.ru>
2024-03-09 20:42:09 +0500

    Эхо бот

--- test.py
+++ test.py
@@ -1,13 +1,37 @@
 import re
 import requests
 import time
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
 
 TOKEN = ""
-API_URL = "https://api.telegram.org/bot"
 with open("secret.txt", "r") as q:
     text = q.read()
     TOKEN = re.match(r'token:([:\w-]*$)', text).group(1)
-for i in range(1000):
-    updates = requests.get(f'{API_URL}{TOKEN}/sendMessage?chat_id=1393291388&text=QQQQQQQ')
-    print(updates.text)
-    time.sleep(1)
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+
+
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer(
+        'Напиши мне что-нибудь и в ответ '
+        'я пришлю тебе твое сообщение'
+    )
+
+
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=message.text)
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
