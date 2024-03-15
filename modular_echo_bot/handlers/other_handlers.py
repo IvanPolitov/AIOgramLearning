7151f0a5deb2de556de661ef844315fd3fcf4757
IvanPolitov Work <ivan.hanter21@mail.ru>
2024-03-15 19:08:35 +0500

    Асинхронный эхо-бот, структура проекта, роутеры

--- modular_echo_bot/handlers/other_handlers.py
+++ modular_echo_bot/handlers/other_handlers.py
@@ -3,3 +3,17 @@
 который будет обрабатывать сообщения,
 не предусмотренные в рамках общения пользователя с ботом
 '''
+from aiogram import Router
+from aiogram.types import Message
+from lexicon.lexicon import LEXICON_RU
+
+
+router = Router()
+
+
+@router.message()
+async def send_echo(message: Message):
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(text=LEXICON_RU['no_echo'])
